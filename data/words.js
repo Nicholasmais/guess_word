aa945b6f02d68dc093308a7d356fbbbccb5b9028
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-08-15 13:16:56 -0300

    removido arquivos padroes

--- data/words.js
+++ data/words.js
@@ -1,8 +1,8 @@
 export const wordsList = {
-  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React"],
-  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena","Retrovisor","Cinto","Banco"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja","Melão","Abacaxi","Melancia"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos","Joelho","Barriga","Tornozelo","Panturrilha"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete","Processador","Memória","Transistor","Microcontrolador","Circuito"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React","Python","Java","Julia","C++","PHP","Golang","Cobol","Assembly","Django","Flask","Spring"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo","Bacon","Sanduíche","Presunto","Queijo"],
 };
