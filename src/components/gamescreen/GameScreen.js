74fc1a4f3609687a38b1d5907ef7b20ff3723655
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-08-17 18:05:40 -0300

    Inicio de game

--- src/components/gamescreen/GameScreen.js
+++ src/components/gamescreen/GameScreen.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import s from "./gamescreen.module.scss";
 
 const GameScreen = ({verifyLetter, randomPick}) => {
     let {pickedCategory, pickedWord} = randomPick;
     return (
-    <div>GameScreen
-        <h1>{pickedCategory}</h1>
-        <h3>{pickedWord}</h3>
-        <button onClick={verifyLetter}>Terminar Jogo</button>
+    <div className={s.divBody}>
+        <div className={s.points}><span>Pontuação = {0}</span></div>
+
+        <h1 className={s.h1}>Adivinhe a palavra:</h1>
+        <h3 className={s.h3}>Dica: <span>{pickedCategory}</span></h3>
+
+        <div className={s.wordContainer}>
+            <span className={s.letter}>A</span>
+            <span className={s.blankSquare}>B</span>
+        </div>
+
+        <div className={s.letterContainer}>
+            <p>Tente adivinhar uma letra:</p>
+            <form >
+                <input type="text" name='letter' maxLength="1" required/>
+                <button>Adivinhar</button>
+            </form>
+        </div>
+
+        <div className={s.wrongLetterContainer}>
+            <p>Letra erradas:</p>
+            <span>a, b, c, d</span>
+        </div>
+        <div>
+        <button onClick={s.verifyLetter}>Terminar Jogo</button>
+        </div>
     </div>
   )
 }
